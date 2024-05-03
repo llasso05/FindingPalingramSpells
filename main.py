a6528227422f1360a4b8782d4c8dc6b5e2448ae0
LuigiLasso <llasso05@gmail.com>
2024-05-03 12:05:00 -0500

    adding palindrom search functionality, moved read dictionary to functions package

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import sys
+from functions.loader import load_dictionary
 
-# open file function
-def open_dictionary(file):
-    try:
-        with open(file) as in_file:
-            loaded_text = in_file.read().strip().split('\n')
-            loaded_text = [x.lower() for x in loaded_text]
-            return loaded_text
-    except IOError as e:
-        print(f'{e} \n error opening {in_file}. Terminating Program',
-              file = sys.stderr)
-        sys.exit(1)
+palindromes = []
 
-file_text = open_dictionary('iventWithPythonDict.txt')
-print(file_text)
+def main():
+    dictionary = input("Please insert the dictionary file name ")
+    word_list = load_dictionary(dictionary)
+    for word in word_list:
+        if  len(word) > 1 and word == word[::-1]:
+            palindromes.append(word)
+            print(f'the word "{word}" had been added to palindromes list\n')
+    print(f'Total palindromes added {len(palindromes)}')
+    print(*palindromes, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
+
+    
+
+
---
+++ test.py
@@ -0,0 +1,10 @@
+word = 'nurses'
+
+word2 = word[2:]
+word3 = word[:-1]
+word4 = word[::-1]
+
+print(word)
+print(word2)
+print(word3)
+print(word4)
